542595c77275b44ba5f891c991d9020cba099fd9
Rasel Kibria <rasel5jkibria@gmail.com>
2021-10-28 01:36:06 +0600

    login page added, LoginController created, route added

--- computerShop/routes/web.php
+++ computerShop/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.login');
 });
+Route::get('/login',[LoginController::class,'login'])->name('login');
